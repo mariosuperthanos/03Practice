d1b0b30709962d8d45b88b80562ec8611f843b15
david <mario.bargianu@gmail.com>
2025-02-22 19:49:51 +0200

    lint errors solved

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable no-undef */
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
+import React from 'react';
 
 describe('MainContent', () => {
   it('should render a button', () => {
